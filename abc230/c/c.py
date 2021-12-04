6a3c398c4de623487320d93b7ca0be76a4ec5b4a
Shuhei Abe <blueletter123456789@gmail.com>
2021-12-04 14:16:33 +0900

    moidfy to answer 'abc230'

--- abc230/c/c.py
+++ abc230/c/c.py
@@ -1,19 +1,14 @@
 n, a, b = map(int, input().split())
 p, q, r, s = map(int, input().split())
-max_a = max(1-a, 1-b)
-min_a = min(n-a, n-b)
-max_b = max(1-a, b-n)
-min_b = min(n-a, b-1)
-m = []
-for k in range(max_a, min_a+1):
-    m.append([k+a, k+b])
-for k in range(max_b, min_b+1):
-    m.append([k+a, b-k])
-for i in range(p, q+1):
-    l = []
-    for j in range(r, s+1):
-        if [i,j] in m:
-            l.append('#')
-        else:
-            l.append('.')
-    print(''.join(l))
+ans = [['.' for _ in range(r, s+1)] for _ in range(p, q+1)]
+max_a = max(p-a, r-b)
+min_a = min(q-a, s-b)
+for i in range(max_a, min_a+1):
+    ans[a+i-p][b+i-r] = '#'
+max_b = max(p-a, b-s)
+min_b = min(q-a, b-r)
+for i in range(max_b, min_b+1):
+    ans[a+i-p][b-i-r] = '#'
+for i in range(len(ans)):
+    s = ''.join(ans[i])
+    print(''.join(ans[i]))
