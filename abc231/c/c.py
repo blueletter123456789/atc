85d16ce4ecd77122a8f43020a6cb4a9325b3d486
Shuhei Abe <blueletter123456789@gmail.com>
2021-12-16 21:38:18 +0900

    solved 'ABC231c'

--- abc231/c/c.py
+++ abc231/c/c.py
@@ -1,26 +1,26 @@
-# def search(nums, val, n):
-#     left, right = -1, n
-#     while right - left > 1:
-#         mid = (left + right) // 2
-#         if nums[mid] < val:
-#             left = mid
-#         else:
-#             right = mid
-#     return n-mid
-
-# n, q = map(int, input().split())
-# l = list(map(int, input().split()))
-# l.sort()
-# for _ in range(q):
-#     t = int(input())
-#     print(search(l, t, n))
-
-import bisect
+def search(nums, val, n):
+    left, right = 0, n
+    while right > left:
+        mid = (left + right) // 2
+        if nums[mid] < val:
+            left = mid + 1
+        else:
+            right = mid
+    return n-left
 
 n, q = map(int, input().split())
 l = list(map(int, input().split()))
 l.sort()
 for _ in range(q):
     t = int(input())
-    i = bisect.bisect_left(l, t)
-    print(n-i)
+    print(search(l, t, n))
+
+# import bisect
+
+# n, q = map(int, input().split())
+# l = list(map(int, input().split()))
+# l.sort()
+# for _ in range(q):
+#     t = int(input())
+#     i = bisect.bisect_left(l, t)
+#     print(n-i)
