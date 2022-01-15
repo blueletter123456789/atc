f0fe104b1fed569fcd5aa2812f0b3f1e9dc046cb
Shuhei Abe <blueletter123456789@gmail.com>
2022-01-15 13:33:47 +0900

    ABC234d answer

--- abc234/d/d.py
+++ abc234/d/d.py
@@ -1,19 +1,23 @@
-def main(n, k, l):
-    t = sorted(l[:k], reverse=True)
-    for i in range(k, n+1):
-        # t = sorted(l[:i], reverse=True)
-        if k != i:
-            flg = True
-            for j in range(len(t)):
-                if t[j] < l[i-1]:
-                    t.insert(j, l[i-1])
-                    flg = False
-                    break
-            if flg:
-                t.append(l[i-1])
-        print(t[k-1])
+# def main(n, k, l):
+#     for i in range(k, n+1):
+#         t = sorted(l[:i], reverse=True)
+#         print(t[k-1])
+ 
+# if __name__ == '__main__':
+#     n, k = map(int, input().split())
+#     l = list(map(int, input().split()))
+#     main(n, k, l)
 
-if __name__ == '__main__':
-    n, k = map(int, input().split())
-    l = list(map(int, input().split()))
-    main(n, k, l)
+import heapq
+N,K = map(int,input().split())
+P = list(map(int,input().split()))
+que = P[0:K]
+print(min(que))
+heapq.heapify(que)
+for i in range(K,N):
+    minima = heapq.heappop(que)
+    minima = max(minima,P[i])
+    heapq.heappush(que,minima)
+    ans = heapq.heappop(que)
+    print(ans)
+    heapq.heappush(que,ans)
